f2647dbffc50b5c14270b152b0ed0aef89d91a24
unknown <asko2212@hotmail.com>
2022-01-14 10:16:31 +0100

    edit posao jedan dio zavrsen

--- components/posao/FormaPosao.jsx
+++ components/posao/FormaPosao.jsx
@@ -1,117 +1,133 @@
-import { useState } from 'react'
-import { useFocus } from '../../hooks/useFocus'
+import { data } from 'autoprefixer';
+import { useState } from 'react';
+import { useFocus } from '../../hooks/useFocus';
 import {
-  useAddMamaAktivnostMutation,
-  useAddPosaoMutation,
-} from '../../redux/apiQuery'
+    useAddMamaAktivnostMutation,
+    useAddPosaoMutation,
+    useGetAllPosaoQuery,
+} from '../../redux/apiQuery';
 
-const FormaRacun = ({ referenca }) => {
-  const [formData, setFormData] = useState({
-    naziv: '',
-    opis: '',
-    zavrsen: false,
-  })
+const FormaRacun = ({ referenca, isEditing, setIsEditing, editedItem }) => {
+    const [formData, setFormData] = useState({
+        naziv: '',
+        opis: '',
+        zavrsen: false,
+    });
 
-  const [inputRef, setInputFocus] = useFocus()
+    const [inputRef, setInputFocus] = useFocus();
 
-  const [greska, setGreska] = useState(null)
+    const [greska, setGreska] = useState(null);
 
-  const [dodajPosao, isLoading] = useAddPosaoMutation()
-  const [dodajMamaAktivnost, isLoadingMama] = useAddMamaAktivnostMutation()
+    const [dodajPosao, isLoading] = useAddPosaoMutation();
+    const [dodajMamaAktivnost, isLoadingMama] = useAddMamaAktivnostMutation();
 
-  const handleChange = (e) => {
-    const name = e.target.name
-    const value =
-      e.target.type === 'checkbox' ? e.target.checked : e.target.value
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }))
-  }
+    const handleChange = (e) => {
+        const name = e.target.name;
+        const value =
+            e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        setFormData((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    if (formData.naziv === '' || formData.opis === '') {
-      setGreska('Popunite sva polja')
-      setTimeout(() => {
-        setGreska(false)
-      }, 3000)
-      return
-    }
-    setFormData({ naziv: '', opis: '', zavrsen: false })
-    if (referenca === 'mama') {
-      await dodajMamaAktivnost(formData).unwrap()
-    } else {
-      await dodajPosao(formData).unwrap()
-    }
-    setInputFocus()
-  }
+    const { posaoState } = useGetAllPosaoQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            posaoState: data?.posao.find(
+                (posao) => posao._id === editedItem._id
+            ),
+        }),
+    });
 
-  return (
-    <div className='w-full max-w-xs'>
-      <form
-        onSubmit={handleSubmit}
-        className='px-8 pt-6 pb-8 mt-4 mb-4 bg-white rounded-lg shadow-md'
-      >
-        <div className='mb-4'>
-          <label
-            htmlFor='naziv'
-            className='block mb-2 text-sm font-bold text-gray-700'
-          >
-            Naziv aktivnosti
-          </label>
-          <input
-            value={formData.naziv}
-            onChange={handleChange}
-            name='naziv'
-            id='posao'
-            type='text'
-            ref={inputRef}
-            className='block w-full px-4 py-2 pr-8 leading-tight bg-white border border-gray-400 rounded shadow appearance-none hover:border-gray-500 focus:outline-none focus:shadow-outline'
-          />
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (formData.naziv === '' || formData.opis === '') {
+            setGreska('Popunite sva polja');
+            setTimeout(() => {
+                setGreska(false);
+            }, 3000);
+            return;
+        }
+        setFormData({ naziv: '', opis: '', zavrsen: false });
+        if (referenca === 'mama') {
+            await dodajMamaAktivnost(formData).unwrap();
+        } else {
+            await dodajPosao(formData).unwrap();
+        }
+        setInputFocus();
+    };
+
+    return (
+        <div className='w-full max-w-xs'>
+            <form
+                onSubmit={handleSubmit}
+                className='px-8 pt-6 pb-8 mt-4 mb-4 bg-white rounded-lg shadow-md'
+            >
+                <div className='mb-4'>
+                    <label
+                        htmlFor='naziv'
+                        className='block mb-2 text-sm font-bold text-gray-700'
+                    >
+                        Naziv aktivnosti
+                    </label>
+                    <input
+                        value={isEditing ? editedItem?.naziv : formData.naziv}
+                        onChange={handleChange}
+                        name='naziv'
+                        id='posao'
+                        type='text'
+                        ref={inputRef}
+                        className='block w-full px-4 py-2 pr-8 leading-tight bg-white border border-gray-400 rounded shadow appearance-none hover:border-gray-500 focus:outline-none focus:shadow-outline'
+                    />
+                </div>
+                <div className='mb-4'>
+                    <label
+                        htmlFor='iznos'
+                        className='block mb-2 text-sm font-bold text-gray-700'
+                    >
+                        Opis
+                    </label>
+                    <textarea
+                        value={isEditing ? editedItem?.opis : formData.opis}
+                        onChange={handleChange}
+                        type='text'
+                        className='block w-full px-4 py-2 pr-8 leading-tight bg-white border border-gray-400 rounded shadow appearance-none h-28 hover:border-gray-500 focus:outline-none focus:shadow-outline'
+                        id='posao'
+                        name='opis'
+                    />
+                </div>
+                <div className='flex items-center justify-between mb-6'>
+                    <label
+                        htmlFor='zavrsen'
+                        className='block mb-2 text-sm font-bold text-gray-700'
+                    >
+                        Završen
+                    </label>
+                    <input
+                        value={
+                            isEditing ? editedItem?.zavrsen : formData.zavrsen
+                        }
+                        onChange={handleChange}
+                        name='zavrsen'
+                        id='posao'
+                        type='checkbox'
+                        className='w-5 h-5'
+                    />
+                </div>
+                <button
+                    disabled={
+                        referenca === 'mama' ? !isLoadingMama : !isLoading
+                    }
+                    className='btn btn-wide disabled:bg-gray-200 disabled:loading'
+                >
+                    {isEditing ? 'Ispravi' : 'Potvrdi'}
+                </button>
+                {greska && (
+                    <p className='mt-2 text-center text-red-500'>{greska}</p>
+                )}
+            </form>
         </div>
-        <div className='mb-4'>
-          <label
-            htmlFor='iznos'
-            className='block mb-2 text-sm font-bold text-gray-700'
-          >
-            Opis
-          </label>
-          <textarea
-            value={formData.opis}
-            onChange={handleChange}
-            type='text'
-            className='block w-full px-4 py-2 pr-8 leading-tight bg-white border border-gray-400 rounded shadow appearance-none h-28 hover:border-gray-500 focus:outline-none focus:shadow-outline'
-            id='posao'
-            name='opis'
-          />
-        </div>
-        <div className='flex items-center justify-between mb-6'>
-          <label
-            htmlFor='zavrsen'
-            className='block mb-2 text-sm font-bold text-gray-700'
-          >
-            Završen
-          </label>
-          <input
-            value={formData.zavrsen}
-            onChange={handleChange}
-            name='zavrsen'
-            id='posao'
-            type='checkbox'
-            className='w-5 h-5'
-          />
-        </div>
-        <button
-          disabled={referenca === 'mama' ? !isLoadingMama : !isLoading}
-          className='btn btn-wide disabled:bg-gray-200 disabled:loading'
-        >
-          Potvrdi
-        </button>
-        {greska && <p className='mt-2 text-center text-red-500'>{greska}</p>}
-      </form>
-    </div>
-  )
-}
+    );
+};
 
-export default FormaRacun
+export default FormaRacun;
--- components/posao/PosaoList.jsx
+++ components/posao/PosaoList.jsx
@@ -1,99 +1,115 @@
-import { TiDocumentDelete } from 'react-icons/ti'
+import { TiDocumentDelete, TiDocumentText } from 'react-icons/ti';
 import {
-  useGetAllMamaAktivnostiQuery,
-  useGetAllPosaoQuery,
-  useIzbrisiMamaAktivnostMutation,
-  useIzbrisiPosaoMutation,
-  useToggleZavrsenMamaMutation,
-  useToggleZavrsenMutation,
-} from '../../redux/apiQuery'
-import dayjs from 'dayjs'
-import Swal from 'sweetalert2'
-import withReactContent from 'sweetalert2-react-content'
+    useGetAllMamaAktivnostiQuery,
+    useGetAllPosaoQuery,
+    useIzbrisiMamaAktivnostMutation,
+    useIzbrisiPosaoMutation,
+    useToggleZavrsenMamaMutation,
+    useToggleZavrsenMutation,
+} from '../../redux/apiQuery';
+import dayjs from 'dayjs';
+import Swal from 'sweetalert2';
+import withReactContent from 'sweetalert2-react-content';
 
-const PosaoList = ({ data, id, referenca }) => {
-  const datum = dayjs(data?.createdAt).format('DD.MM.YYYY')
-  const vrijeme = dayjs(data?.createdAt).format('HH:mm')
+const PosaoList = ({
+    data,
+    id,
+    referenca,
+    isEditing,
+    setIsEditing,
+    editedItem,
+    setEditedItem,
+}) => {
+    const datum = dayjs(data?.createdAt).format('DD.MM.YYYY');
+    const vrijeme = dayjs(data?.createdAt).format('HH:mm');
 
-  let zavrsenClassName =
-    'flex border-l-8 items-center gap-10 rounded-lg relative mb-4 cursor-pointer'
-  if (data?.zavrsen) {
-    zavrsenClassName += ' border-green-400 shadow-lg shadow-green-400/40'
-  } else {
-    zavrsenClassName += ' border-red-400 shadow-lg shadow-red-400/40'
-  }
-  const { refetch } = useGetAllPosaoQuery()
-
-  const { posaoToggle } = useGetAllPosaoQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      posaoToggle: data?.posao.find((posao) => posao._id === id),
-    }),
-  })
-  const { mamaToggle } = useGetAllMamaAktivnostiQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      mamaToggle: data?.mama.find((mama) => mama._id === id),
-    }),
-  })
-  const [izbrisiPosao] = useIzbrisiPosaoMutation()
-  const [izmijeniZavrsen, result] = useToggleZavrsenMutation({
-    fixedCacheKey: 'shared-update-post',
-  })
-  const [izbrisiMamaAktivnost] = useIzbrisiMamaAktivnostMutation()
-  const [izmijeniZavrsenMama] = useToggleZavrsenMamaMutation({
-    fixedCacheKey: 'shared-update-post',
-  })
-
-  const toggleZavrsen = async () => {
-    if (referenca === 'mama') {
-      await izmijeniZavrsenMama({ id, zavrsen: !mamaToggle?.zavrsen })
+    let zavrsenClassName =
+        'flex border-l-8 items-center gap-10 rounded-lg relative mb-4 cursor-pointer';
+    if (data?.zavrsen) {
+        zavrsenClassName += ' border-green-400 shadow-lg shadow-green-400/40';
     } else {
-      await izmijeniZavrsen({ id, zavrsen: !posaoToggle?.zavrsen })
+        zavrsenClassName += ' border-red-400 shadow-lg shadow-red-400/40';
     }
-  }
+    const { refetch } = useGetAllPosaoQuery();
 
-  const handleDelete = async (id) => {
-    const MySwal = withReactContent(Swal)
-    const confirm = await MySwal.fire({
-      title: 'Jeste li sigurni?',
-      text: 'Želite obrisati aktivnost?',
-      icon: 'warning',
-      showDenyButton: true,
-      confirmButtonText: 'Da',
-      denyButtonText: 'Ne',
-      customClass: { htmlContainer: 'grid-row:1' },
-    })
-    if (confirm.isConfirmed) {
-      Swal.fire('Aktivnost izbrisana!', '', 'success')
-      if (referenca === 'mama') {
-        izbrisiMamaAktivnost({ id }).unwrap()
-      } else {
-        izbrisiPosao({ id }).unwrap()
-      }
-      refetch()
-    } else {
-      Swal.fire('Aktivnost nije izbrisana', '', 'info')
-    }
-  }
+    const { posaoToggle } = useGetAllPosaoQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            posaoToggle: data?.posao.find((posao) => posao._id === id),
+        }),
+    });
+    const { mamaToggle } = useGetAllMamaAktivnostiQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            mamaToggle: data?.mama.find((mama) => mama._id === id),
+        }),
+    });
+    const [izbrisiPosao] = useIzbrisiPosaoMutation();
+    const [izmijeniZavrsen, result] = useToggleZavrsenMutation({
+        fixedCacheKey: 'shared-update-post',
+    });
+    const [izbrisiMamaAktivnost] = useIzbrisiMamaAktivnostMutation();
+    const [izmijeniZavrsenMama] = useToggleZavrsenMamaMutation({
+        fixedCacheKey: 'shared-update-post',
+    });
 
-  return (
-    <>
-      <div className={zavrsenClassName} onDoubleClick={toggleZavrsen}>
-        {/* <p className='p-2'>{rb}</p> */}
-        <div className='p-2 border-r-2'>
-          <p className='py-5 font-bold'>{data?.naziv}</p>
-          <time className='block -mb-2 text-sm'>{datum}</time>
-          <time className='text-sm italic '>{vrijeme}h</time>
-        </div>
-        <p>{data?.opis}</p>
-        <TiDocumentDelete
-          className='absolute top-0 text-red-500 transition-all cursor-pointer right-2 active:scale-90'
-          size={25}
-          onClick={() => handleDelete(id)}
-        />
-      </div>
-      {/* <div className='divider'></div> */}
-    </>
-  )
-}
+    const toggleZavrsen = async () => {
+        if (referenca === 'mama') {
+            await izmijeniZavrsenMama({ id, zavrsen: !mamaToggle?.zavrsen });
+        } else {
+            await izmijeniZavrsen({ id, zavrsen: !posaoToggle?.zavrsen });
+        }
+    };
 
-export default PosaoList
+    const handleDelete = async (id) => {
+        const MySwal = withReactContent(Swal);
+        const confirm = await MySwal.fire({
+            title: 'Jeste li sigurni?',
+            text: 'Želite obrisati aktivnost?',
+            icon: 'warning',
+            showDenyButton: true,
+            confirmButtonText: 'Da',
+            denyButtonText: 'Ne',
+            customClass: { htmlContainer: 'grid-row:1' },
+        });
+        if (confirm.isConfirmed) {
+            Swal.fire('Aktivnost izbrisana!', '', 'success');
+            if (referenca === 'mama') {
+                izbrisiMamaAktivnost({ id }).unwrap();
+            } else {
+                izbrisiPosao({ id }).unwrap();
+            }
+            refetch();
+        } else {
+            Swal.fire('Aktivnost nije izbrisana', '', 'info');
+        }
+    };
+
+    const handleEditClick = () => {
+        setIsEditing(true);
+        setEditedItem(data);
+    };
+
+    return (
+        <>
+            <div className={zavrsenClassName} onDoubleClick={toggleZavrsen}>
+                <div className='p-2 border-r-2'>
+                    <p className='py-5 font-bold'>{data?.naziv}</p>
+                    <time className='block -mb-2 text-sm'>{datum}</time>
+                    <time className='text-sm italic '>{vrijeme}h</time>
+                </div>
+                <p>{data?.opis}</p>
+                <TiDocumentDelete
+                    className='absolute top-0 text-red-500 transition-all cursor-pointer right-2 active:scale-90'
+                    size={25}
+                    onClick={() => handleDelete(id)}
+                />
+                <TiDocumentText
+                    className='absolute top-0 text-slate-500 transition-all cursor-pointer right-8 active:scale-90'
+                    size={25}
+                    onClick={handleEditClick}
+                />
+            </div>
+        </>
+    );
+};
+
+export default PosaoList;
