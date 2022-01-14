497e84a9ec94e6bc17f6da667a57a903f6869002
unknown <asko2212@hotmail.com>
2022-01-14 11:39:17 +0100

    edit posao zavrseno

--- components/posao/FormaPosao.jsx
+++ components/posao/FormaPosao.jsx
@@ -1,22 +1,27 @@
-import { data } from 'autoprefixer';
 import { useState } from 'react';
 import { useFocus } from '../../hooks/useFocus';
 import {
     useAddMamaAktivnostMutation,
     useAddPosaoMutation,
     useGetAllPosaoQuery,
+    useUpdatePosaoMutation,
 } from '../../redux/apiQuery';
 
-const FormaRacun = ({ referenca, isEditing, setIsEditing, editedItem }) => {
+const FormaRacun = ({
+    referenca,
+    isEditing,
+    setIsEditing,
+    editedItem,
+    setEditedItem,
+}) => {
     const [formData, setFormData] = useState({
         naziv: '',
         opis: '',
         zavrsen: false,
     });
+    const [greska, setGreska] = useState(null);
 
     const [inputRef, setInputFocus] = useFocus();
-
-    const [greska, setGreska] = useState(null);
 
     const [dodajPosao, isLoading] = useAddPosaoMutation();
     const [dodajMamaAktivnost, isLoadingMama] = useAddMamaAktivnostMutation();
@@ -30,6 +35,15 @@ const FormaRacun = ({ referenca, isEditing, setIsEditing, editedItem }) => {
             [name]: value,
         }));
     };
+    const handleChangeEditItem = (e) => {
+        const name = e.target.name;
+        const value =
+            e.target.type === 'checkbox' ? e.target.checked : e.target.value;
+        setEditedItem((prevData) => ({
+            ...prevData,
+            [name]: value,
+        }));
+    };
 
     const { posaoState } = useGetAllPosaoQuery(undefined, {
         selectFromResult: ({ data }) => ({
@@ -37,6 +51,10 @@ const FormaRacun = ({ referenca, isEditing, setIsEditing, editedItem }) => {
                 (posao) => posao._id === editedItem._id
             ),
         }),
+    });
+
+    const [updatePosao] = useUpdatePosaoMutation({
+        fixedCacheKey: 'shared-update-post',
     });
 
     const handleSubmit = async (e) => {
@@ -56,11 +74,34 @@ const FormaRacun = ({ referenca, isEditing, setIsEditing, editedItem }) => {
         }
         setInputFocus();
     };
+    const handleEditSubmit = async (e) => {
+        e.preventDefault();
+        if (editedItem.naziv === '' || editedItem.opis === '') {
+            setGreska('Popunite sva polja');
+            setTimeout(() => {
+                setGreska(false);
+            }, 3000);
+            return;
+        }
+        if (referenca === 'mama') {
+            await dodajMamaAktivnost(formData).unwrap();
+        } else {
+            await updatePosao({
+                id: posaoState?._id,
+                naziv: editedItem?.naziv,
+                opis: editedItem?.opis,
+                zavrsen: editedItem?.zavrsen,
+            }).unwrap();
+        }
+        // setFormData({ naziv: '', opis: '', zavrsen: false });
+        setIsEditing(false);
+        setInputFocus();
+    };
 
     return (
         <div className='w-full max-w-xs'>
             <form
-                onSubmit={handleSubmit}
+                onSubmit={isEditing ? handleEditSubmit : handleSubmit}
                 className='px-8 pt-6 pb-8 mt-4 mb-4 bg-white rounded-lg shadow-md'
             >
                 <div className='mb-4'>
@@ -72,7 +113,9 @@ const FormaRacun = ({ referenca, isEditing, setIsEditing, editedItem }) => {
                     </label>
                     <input
                         value={isEditing ? editedItem?.naziv : formData.naziv}
-                        onChange={handleChange}
+                        onChange={
+                            isEditing ? handleChangeEditItem : handleChange
+                        }
                         name='naziv'
                         id='posao'
                         type='text'
@@ -89,7 +132,9 @@ const FormaRacun = ({ referenca, isEditing, setIsEditing, editedItem }) => {
                     </label>
                     <textarea
                         value={isEditing ? editedItem?.opis : formData.opis}
-                        onChange={handleChange}
+                        onChange={
+                            isEditing ? handleChangeEditItem : handleChange
+                        }
                         type='text'
                         className='block w-full px-4 py-2 pr-8 leading-tight bg-white border border-gray-400 rounded shadow appearance-none h-28 hover:border-gray-500 focus:outline-none focus:shadow-outline'
                         id='posao'
@@ -107,7 +152,9 @@ const FormaRacun = ({ referenca, isEditing, setIsEditing, editedItem }) => {
                         value={
                             isEditing ? editedItem?.zavrsen : formData.zavrsen
                         }
-                        onChange={handleChange}
+                        onChange={
+                            isEditing ? handleChangeEditItem : handleChange
+                        }
                         name='zavrsen'
                         id='posao'
                         type='checkbox'
--- redux/apiQuery.js
+++ redux/apiQuery.js
@@ -1,88 +1,97 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
-  baseQuery: fetchBaseQuery({
-    baseUrl: `${process.env.NEXT_PUBLIC_MY_URL}/api/`,
-  }),
-  // reducerPath: 'posaoApi',
-  tagTypes: ['Posao', 'Mama'],
-  endpoints: (build) => ({
-    getAllPosao: build.query({
-      query: () => 'posao',
-      providesTags: (result) =>
-        result
-          ? [
-              //   ...result.map(({ id }) => ({ type: 'Posao', id })),
-              { type: 'Posao', id: 'LIST' },
-            ]
-          : [{ type: 'Posao', id: 'LIST' }],
+    baseQuery: fetchBaseQuery({
+        baseUrl: `${process.env.NEXT_PUBLIC_MY_URL}/api/`,
     }),
-    addPosao: build.mutation({
-      query: (body) => ({
-        url: 'posao',
-        method: 'POST',
-        body,
-      }),
-      invalidatesTags: [{ type: 'Posao', id: 'LIST' }],
+    // reducerPath: 'posaoApi',
+    tagTypes: ['Posao', 'Mama'],
+    endpoints: (build) => ({
+        getAllPosao: build.query({
+            query: () => 'posao',
+            providesTags: (result) =>
+                result
+                    ? [
+                          //   ...result.map(({ id }) => ({ type: 'Posao', id })),
+                          { type: 'Posao', id: 'LIST' },
+                      ]
+                    : [{ type: 'Posao', id: 'LIST' }],
+        }),
+        addPosao: build.mutation({
+            query: (body) => ({
+                url: 'posao',
+                method: 'POST',
+                body,
+            }),
+            invalidatesTags: [{ type: 'Posao', id: 'LIST' }],
+        }),
+        izbrisiPosao: build.mutation({
+            query: ({ id }) => ({
+                url: `posao/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: [{ type: 'Posao', id: 'LIST' }],
+        }),
+        updatePosao: build.mutation({
+            query: ({ id, ...rest }) => ({
+                url: `posao/${id}`,
+                method: 'PUT',
+                body: rest,
+            }),
+            invalidatesTags: [{ type: 'Posao', id: 'LIST' }],
+        }),
+        toggleZavrsen: build.mutation({
+            query: ({ id, ...rest }) => ({
+                url: `posao/${id}`,
+                method: 'PUT',
+                body: rest,
+            }),
+            invalidatesTags: [{ type: 'Posao', id: 'LIST' }],
+        }),
+        getAllMamaAktivnosti: build.query({
+            query: () => 'mama',
+            providesTags: (result) =>
+                result
+                    ? [
+                          //   ...result.map(({ id }) => ({ type: 'Posao', id })),
+                          { type: 'Mama', id: 'LIST' },
+                      ]
+                    : [{ type: 'Mama', id: 'LIST' }],
+        }),
+        addMamaAktivnost: build.mutation({
+            query: (body) => ({
+                url: 'mama',
+                method: 'POST',
+                body,
+            }),
+            invalidatesTags: [{ type: 'Mama', id: 'LIST' }],
+        }),
+        izbrisiMamaAktivnost: build.mutation({
+            query: ({ id }) => ({
+                url: `mama/${id}`,
+                method: 'DELETE',
+            }),
+            invalidatesTags: [{ type: 'Mama', id: 'LIST' }],
+        }),
+        toggleZavrsenMama: build.mutation({
+            query: ({ id, ...rest }) => ({
+                url: `mama/${id}`,
+                method: 'PUT',
+                body: rest,
+            }),
+            invalidatesTags: [{ type: 'Mama', id: 'LIST' }],
+        }),
     }),
-    izbrisiPosao: build.mutation({
-      query: ({ id }) => ({
-        url: `posao/${id}`,
-        method: 'DELETE',
-      }),
-      invalidatesTags: [{ type: 'Posao', id: 'LIST' }],
-    }),
-    toggleZavrsen: build.mutation({
-      query: ({ id, ...rest }) => ({
-        url: `posao/${id}`,
-        method: 'PUT',
-        body: rest,
-      }),
-      invalidatesTags: [{ type: 'Posao', id: 'LIST' }],
-    }),
-    getAllMamaAktivnosti: build.query({
-      query: () => 'mama',
-      providesTags: (result) =>
-        result
-          ? [
-              //   ...result.map(({ id }) => ({ type: 'Posao', id })),
-              { type: 'Mama', id: 'LIST' },
-            ]
-          : [{ type: 'Mama', id: 'LIST' }],
-    }),
-    addMamaAktivnost: build.mutation({
-      query: (body) => ({
-        url: 'mama',
-        method: 'POST',
-        body,
-      }),
-      invalidatesTags: [{ type: 'Mama', id: 'LIST' }],
-    }),
-    izbrisiMamaAktivnost: build.mutation({
-      query: ({ id }) => ({
-        url: `mama/${id}`,
-        method: 'DELETE',
-      }),
-      invalidatesTags: [{ type: 'Mama', id: 'LIST' }],
-    }),
-    toggleZavrsenMama: build.mutation({
-      query: ({ id, ...rest }) => ({
-        url: `mama/${id}`,
-        method: 'PUT',
-        body: rest,
-      }),
-      invalidatesTags: [{ type: 'Mama', id: 'LIST' }],
-    }),
-  }),
-})
+});
 
 export const {
-  useGetAllPosaoQuery,
-  useAddPosaoMutation,
-  useIzbrisiPosaoMutation,
-  useToggleZavrsenMutation,
-  useGetAllMamaAktivnostiQuery,
-  useAddMamaAktivnostMutation,
-  useIzbrisiMamaAktivnostMutation,
-  useToggleZavrsenMamaMutation,
-} = api
+    useGetAllPosaoQuery,
+    useAddPosaoMutation,
+    useIzbrisiPosaoMutation,
+    useUpdatePosaoMutation,
+    useToggleZavrsenMutation,
+    useGetAllMamaAktivnostiQuery,
+    useAddMamaAktivnostMutation,
+    useIzbrisiMamaAktivnostMutation,
+    useToggleZavrsenMamaMutation,
+} = api;
