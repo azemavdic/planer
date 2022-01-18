e9215d1fca0cc79fb9e923eeba7ac15736a4978a
unknown <asko2212@hotmail.com>
2022-01-18 14:46:36 +0100

    dodan framer-motion

--- components/layout/Modal.jsx
+++ components/layout/Modal.jsx
@@ -1,42 +1,45 @@
-import { useEffect, useState } from 'react'
-import { createPortal } from 'react-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { isEditing } from '../../redux/editingItemSlice'
-import { showModal } from '../../redux/modalSlice'
+import { useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { isEditing } from '../../redux/editingItemSlice';
+import { showModal } from '../../redux/modalSlice';
 
 const Modal = ({ children, title }) => {
-  const [isBrowser, setIsBrowser] = useState(false)
+    const [isBrowser, setIsBrowser] = useState(false);
 
-  const showModalSelector = useSelector((state) => state.modal.value)
-  const dispatch = useDispatch()
+    const showModalSelector = useSelector((state) => state.modal.value);
+    const dispatch = useDispatch();
 
-  useEffect(() => {
-    setIsBrowser(true)
-  }, [])
+    useEffect(() => {
+        setIsBrowser(true);
+    }, []);
 
-  const handleClose = (e) => {
-    e.preventDefault()
-    dispatch(isEditing(false))
-    dispatch(showModal(false))
-  }
+    const handleClose = (e) => {
+        e.preventDefault();
+        dispatch(isEditing(false));
+        dispatch(showModal(false));
+    };
 
-  const modalContent = showModalSelector ? (
-    <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black/60'>
-      <div className='relative'>
-        <div className='absolute right-4 top-5 font-bold text-xl cursor-pointer'>
-          <a onClick={handleClose}>x</a>
+    const modalContent = showModalSelector ? (
+        <div className='absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black/60'>
+            <div className='relative'>
+                <div className='absolute z-10 right-4 top-5 font-bold text-xl cursor-pointer'>
+                    <a onClick={handleClose}>x</a>
+                </div>
+                {title && <div className=''>{title}</div>}
+                <div className=''>{children}</div>
+            </div>
         </div>
-        {title && <div className=''>{title}</div>}
-        <div className=''>{children}</div>
-      </div>
-    </div>
-  ) : null
+    ) : null;
 
-  if (isBrowser) {
-    return createPortal(modalContent, document.getElementById('modal-root'))
-  } else {
-    return null
-  }
-}
+    if (isBrowser) {
+        return createPortal(
+            modalContent,
+            document.getElementById('modal-root')
+        );
+    } else {
+        return null;
+    }
+};
 
-export default Modal
+export default Modal;
--- pages/mama.jsx
+++ pages/mama.jsx
@@ -1,111 +1,127 @@
-import Head from 'next/head'
-import { useEffect, useState } from 'react'
-import Loading from '../components/layout/Loading'
-import AktivnostiForma from '../components/layout/aktivnosti/AktivnostiForma'
-import AktivnostiLista from '../components/layout/aktivnosti/AktivnostiLista'
-import { useGetAllMamaAktivnostiQuery } from '../redux/apiQuery'
-import DodajButton from '../components/layout/DodajButton'
-import Modal from '../components/layout/Modal'
+import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import Loading from '../components/layout/Loading';
+import AktivnostiForma from '../components/layout/aktivnosti/AktivnostiForma';
+import AktivnostiLista from '../components/layout/aktivnosti/AktivnostiLista';
+import { useGetAllMamaAktivnostiQuery } from '../redux/apiQuery';
+import DodajButton from '../components/layout/DodajButton';
+import Modal from '../components/layout/Modal';
+const { motion } = require('framer-motion');
 const Mama = () => {
-  const [mamaState, setmamaState] = useState(null)
-  const [filterActive, setfilterActive] = useState(false)
-  const [editedItem, setEditedItem] = useState({})
+    const [mamaState, setmamaState] = useState(null);
+    const [filterActive, setfilterActive] = useState(false);
+    const [editedItem, setEditedItem] = useState({});
 
-  const { data, isError, isLoading } = useGetAllMamaAktivnostiQuery()
+    const { data, isError, isLoading } = useGetAllMamaAktivnostiQuery();
 
-  useEffect(() => {
-    setmamaState(data?.mama)
-  }, [data])
+    useEffect(() => {
+        setmamaState(data?.mama);
+    }, [data]);
 
-  const { mamaAktZavrsena } = useGetAllMamaAktivnostiQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      mamaAktZavrsena: data?.mama.filter((mama) => mama?.zavrsen),
-    }),
-  })
-  const { mamaAktNezavrsena } = useGetAllMamaAktivnostiQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      mamaAktNezavrsena: data?.mama.filter((mama) => mama?.zavrsen === false),
-    }),
-  })
+    const { mamaAktZavrsena } = useGetAllMamaAktivnostiQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            mamaAktZavrsena: data?.mama.filter((mama) => mama?.zavrsen),
+        }),
+    });
+    const { mamaAktNezavrsena } = useGetAllMamaAktivnostiQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            mamaAktNezavrsena: data?.mama.filter(
+                (mama) => mama?.zavrsen === false
+            ),
+        }),
+    });
 
-  const styleBadge = 'p-4 border-none cursor-pointer badge shadow-lg'
-  const styleBadgeZavrsen = `${styleBadge} bg-green-700 shadow-green-700/40`
-  const styleBadgeNezavrsen = `${styleBadge} bg-red-600 shadow-red-600/40`
+    const styleBadge = 'p-4 border-none cursor-pointer badge shadow-lg';
+    const styleBadgeZavrsen = `${styleBadge} bg-green-700 shadow-green-700/40`;
+    const styleBadgeNezavrsen = `${styleBadge} bg-red-600 shadow-red-600/40`;
 
-  const filterNezavrseni = () => {
-    setmamaState(mamaAktNezavrsena)
-    setfilterActive(true)
-  }
-  const filterZavrseni = () => {
-    setmamaState(mamaAktZavrsena)
-    setfilterActive(true)
-  }
+    const filterNezavrseni = () => {
+        setmamaState(mamaAktNezavrsena);
+        setfilterActive(true);
+    };
+    const filterZavrseni = () => {
+        setmamaState(mamaAktZavrsena);
+        setfilterActive(true);
+    };
 
-  const ocistiFilter = () => {
-    setmamaState(data?.mama)
-    setfilterActive(false)
-  }
+    const ocistiFilter = () => {
+        setmamaState(data?.mama);
+        setfilterActive(false);
+    };
 
-  return (
-    <div className='relative'>
-      <Head>
-        <title>Planer - Mama</title>
-      </Head>
-      <div className='flex items-center justify-center mt-5 space-x-4'>
-        <p className={styleBadgeNezavrsen} onClick={filterNezavrseni}>
-          Nezavršene: <span className='ml-2'>{mamaAktNezavrsena?.length}</span>
-        </p>
-        <p className={styleBadgeZavrsen} onClick={filterZavrseni}>
-          Završene: <span className='ml-2'>{mamaAktZavrsena?.length}</span>
-        </p>
-        {filterActive && (
-          <p
-            className='p-3 text-center cursor-pointer badge'
-            onClick={ocistiFilter}
-          >
-            Očisti
-          </p>
-        )}
-      </div>
-      <div className='flex items-center justify-center gap-4 mt-5'>
-        <div className='lg:w-9/12 w-full'>
-          <h3 className='text-xl font-bold'>Pregled aktivnosti</h3>
-          {data?.mama.length === 0 ? (
-            <div className='flex items-center justify-center h-[25rem]'>
-              <p>Nema aktivnosti za prikazati.</p>
+    return (
+        <div className='relative'>
+            <Head>
+                <title>Planer - Mama</title>
+            </Head>
+            <div className='flex items-center justify-center mt-5 space-x-4'>
+                <p className={styleBadgeNezavrsen} onClick={filterNezavrseni}>
+                    Nezavršene:{' '}
+                    <span className='ml-2'>{mamaAktNezavrsena?.length}</span>
+                </p>
+                <p className={styleBadgeZavrsen} onClick={filterZavrseni}>
+                    Završene:{' '}
+                    <span className='ml-2'>{mamaAktZavrsena?.length}</span>
+                </p>
+                {filterActive && (
+                    <p
+                        className='p-3 text-center cursor-pointer badge'
+                        onClick={ocistiFilter}
+                    >
+                        Očisti
+                    </p>
+                )}
             </div>
-          ) : isLoading ? (
-            <div className='flex items-center justify-center h-[25rem]'>
-              <Loading />
+            <div className='flex items-center justify-center gap-4 mt-5'>
+                <div className='lg:w-9/12 w-full'>
+                    <h3 className='text-xl font-bold'>Pregled aktivnosti</h3>
+                    {data?.mama.length === 0 ? (
+                        <div className='flex items-center justify-center h-[25rem]'>
+                            <p>Nema aktivnosti za prikazati.</p>
+                        </div>
+                    ) : isLoading ? (
+                        <div className='flex items-center justify-center h-[25rem]'>
+                            <Loading />
+                        </div>
+                    ) : (
+                        <div className='p-4 w-full overflow-y-visible overflow-x-auto bg-white shadow-lg h-[25rem] mt-4 rounded-lg'>
+                            {mamaState?.map((mama) => (
+                                <motion.div
+                                    key={mama._id}
+                                    initial={{ opacity: 0 }}
+                                    whileInView={{ opacity: 1 }}
+                                    viewport={{ once: true }}
+                                >
+                                    <AktivnostiLista
+                                        key={mama._id}
+                                        id={mama._id}
+                                        data={mama}
+                                        referenca='mama'
+                                        editedItem={editedItem}
+                                        setEditedItem={setEditedItem}
+                                    />
+                                </motion.div>
+                            ))}
+                        </div>
+                    )}
+                </div>
             </div>
-          ) : (
-            <div className='p-4 w-full overflow-y-visible overflow-x-auto bg-white shadow-lg h-[25rem] mt-4 rounded-lg'>
-              {mamaState?.map((mama) => (
-                <AktivnostiLista
-                  key={mama._id}
-                  id={mama._id}
-                  data={mama}
-                  referenca='mama'
-                  editedItem={editedItem}
-                  setEditedItem={setEditedItem}
-                />
-              ))}
-            </div>
-          )}
+            <DodajButton />
+            <Modal>
+                <motion.div
+                    initial={{ y: '-100%' }}
+                    animate={{ x: 0, y: 0 }}
+                    className='flex items-center justify-center'
+                >
+                    <AktivnostiForma
+                        editedItem={editedItem}
+                        setEditedItem={setEditedItem}
+                        referenca='mama'
+                    />
+                </motion.div>
+            </Modal>
         </div>
-      </div>
-      <DodajButton />
-      <Modal>
-        <div className='flex items-center justify-center'>
-          <AktivnostiForma
-            editedItem={editedItem}
-            setEditedItem={setEditedItem}
-            referenca='mama'
-          />
-        </div>
-      </Modal>
-    </div>
-  )
-}
+    );
+};
 
-export default Mama
+export default Mama;
