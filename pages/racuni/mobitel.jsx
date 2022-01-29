3a9fc986a681e266b8dae657f959a16e63f1c5e3
azemavdic <asko2212@hotmail.com>
2022-01-29 13:59:41 +0100

    zavrsena stranice za svaki racun

--- pages/racuni/mobitel.jsx
+++ pages/racuni/mobitel.jsx
@@ -1,14 +1,109 @@
 import { useRouter } from 'next/router'
+import DodajButton from '../../components/layout/DodajButton'
+import Modal from '../../components/layout/Modal'
+import Forma from '../../components/layout/racun/Forma'
+import {
+  useIzbrisiMobitelMutation,
+  useGetMobitelQuery,
+} from '../../redux/api/mobitelApi'
+import { BsFillTrashFill, BsFillPencilFill } from 'react-icons/bs'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
+import { showModal } from '../../redux/modalSlice'
+import { useDispatch } from 'react-redux'
+import { useState } from 'react'
+import { isEditing } from '../../redux/editingItemSlice'
+import Loading from '../../components/layout/Loading'
 
 const Mobitel = () => {
+  const [editedItem, setEditedItem] = useState({})
+
   const router = useRouter()
+  const dispatch = useDispatch()
+
+  const { data, isLoading } = useGetMobitelQuery()
+  const [izbrisiRacun] = useIzbrisiMobitelMutation()
+
+  const handleDelete = async (id) => {
+    const MySwal = withReactContent(Swal)
+    const confirm = await MySwal.fire({
+      title: 'Jeste li sigurni?',
+      text: 'Želite obrisati račun?',
+      icon: 'warning',
+      showDenyButton: true,
+      confirmButtonText: 'Da',
+      denyButtonText: 'Ne',
+      customClass: { htmlContainer: 'grid-row:1' },
+    })
+    if (confirm.isConfirmed) {
+      Swal.fire('Račun obrisan!', '', 'success')
+      izbrisiRacun({ id })
+    } else {
+      Swal.fire('Račun nije obrisan.', '', 'info')
+    }
+  }
+
+  const handleEditClick = (id) => {
+    dispatch(showModal(true))
+    dispatch(isEditing(true))
+    setEditedItem(data?.mobitel.find((racun) => racun._id === id))
+  }
+
+  let rb = 1
+
   return (
-    <div>
+    <>
       <button className='btn btn-ghost' onClick={() => router.back()}>
         Nazad
       </button>
-      <p>Mobitel</p>
-    </div>
+      <h2 className='mb-5 text-lg font-bold'>Mobitel</h2>
+      <div className='w-full p-2 text-white rounded bg-slate-600'>
+        <table className='w-full'>
+          <thead className='border-b-2'>
+            <tr>
+              <th>RB</th>
+              <th>Mjesec</th>
+              <th>Iznos</th>
+              <th>Akcije</th>
+            </tr>
+          </thead>
+          <tbody className='text-center'>
+            {isLoading && (
+              <tr>
+                <td>
+                  <Loading item='badge' />
+                </td>
+              </tr>
+            )}
+            {data?.mobitel.map((racun) => (
+              <tr key={racun?._id} className='border-b-[1px]'>
+                <td className='p-2'>{rb++}</td>
+                <td>{racun?.mjesec}</td>
+                <td>{racun?.iznos} KM</td>
+                <td className='space-x-2'>
+                  <button
+                    className='btn btn-info btn-sm'
+                    onClick={() => handleEditClick(racun._id)}
+                  >
+                    <BsFillPencilFill />
+                  </button>
+                  <button
+                    className='btn btn-error btn-sm'
+                    onClick={() => handleDelete(racun._id)}
+                  >
+                    <BsFillTrashFill />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <DodajButton />
+      <Modal>
+        <Forma editedItem={editedItem} setEditedItem={setEditedItem} />
+      </Modal>
+    </>
   )
 }
 
