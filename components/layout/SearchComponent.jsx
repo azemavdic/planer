ab780391eeb90b735a3467b4a53da5c316044bc3
unknown <asko2212@hotmail.com>
2022-02-23 07:29:17 +0100

    ispravljen zadnji mjesec računi card

--- components/layout/SearchComponent.jsx
+++ components/layout/SearchComponent.jsx
@@ -1,113 +1,112 @@
-import { useEffect } from 'react'
-import { useState } from 'react'
-import { useDispatch } from 'react-redux'
-import { useGetAllMamaAktivnostiQuery } from '../../redux/api/mamaApi'
-import { showPretraga, textPretrage } from '../../redux/pretragaSlice'
-import { useRouter } from 'next/router'
-import { useGetAllPosaoQuery } from '../../redux/api/posaoApi'
-import { useGetAllKucaAktivnostiQuery } from '../../redux/api/kucaApi'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useGetAllMamaAktivnostiQuery } from '../../redux/api/mamaApi';
+import { showPretraga, textPretrage } from '../../redux/pretragaSlice';
+import { useRouter } from 'next/router';
+import { useGetAllPosaoQuery } from '../../redux/api/posaoApi';
+import { useGetAllKucaAktivnostiQuery } from '../../redux/api/kucaApi';
 
 const SearchComponent = () => {
-  const [text, setText] = useState('')
+    const [text, setText] = useState('');
 
-  const router = useRouter()
+    const router = useRouter();
 
-  const { mamaAkt } = useGetAllMamaAktivnostiQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      mamaAkt: data?.user?.mama.filter((akt) => {
-        if (text) {
-          if (
-            akt?.naziv.toLowerCase().includes(text.toLowerCase()) ||
-            akt?.opis.toLowerCase().includes(text.toLowerCase())
-          ) {
-            return akt
-          }
+    const { mamaAkt } = useGetAllMamaAktivnostiQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            mamaAkt: data?.user?.mama.filter((akt) => {
+                if (text) {
+                    if (
+                        akt?.naziv.toLowerCase().includes(text.toLowerCase()) ||
+                        akt?.opis.toLowerCase().includes(text.toLowerCase())
+                    ) {
+                        return akt;
+                    }
+                }
+            }),
+        }),
+    });
+
+    const { posaoAkt } = useGetAllPosaoQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            posaoAkt: data?.user?.posao.filter((akt) => {
+                if (text) {
+                    if (
+                        akt?.naziv.toLowerCase().includes(text.toLowerCase()) ||
+                        akt?.opis.toLowerCase().includes(text.toLowerCase())
+                    ) {
+                        return akt;
+                    }
+                }
+            }),
+        }),
+    });
+
+    const { kucaAkt } = useGetAllKucaAktivnostiQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            kucaAkt: data?.user?.kuca.filter((akt) => {
+                if (text) {
+                    if (
+                        akt?.naziv.toLowerCase().includes(text.toLowerCase()) ||
+                        akt?.opis.toLowerCase().includes(text.toLowerCase())
+                    ) {
+                        return akt;
+                    }
+                }
+            }),
+        }),
+    });
+
+    const dispatch = useDispatch();
+
+    const handleChange = (e) => {
+        setText(e.target.value);
+        switch (router.pathname) {
+            case '/mama':
+                dispatch(showPretraga(mamaAkt));
+                break;
+            case '/kuca':
+                dispatch(showPretraga(kucaAkt));
+                break;
+            case '/posao':
+                dispatch(showPretraga(posaoAkt));
+                break;
+            default:
+                break;
         }
-      }),
-    }),
-  })
+    };
 
-  const { posaoAkt } = useGetAllPosaoQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      posaoAkt: data?.user?.posao.filter((akt) => {
-        if (text) {
-          if (
-            akt?.naziv.toLowerCase().includes(text.toLowerCase()) ||
-            akt?.opis.toLowerCase().includes(text.toLowerCase())
-          ) {
-            return akt
-          }
-        }
-      }),
-    }),
-  })
+    useEffect(() => {
+        dispatch(textPretrage(text));
+        // if (text === '') {
+        //   switch (router.pathname) {
+        //     case '/mama':
+        //       dispatch(showPretraga(mamaAkt))
+        //       break
+        //     case '/kuca':
+        //       break
+        //     case '/posao':
+        //       break
+        //     default:
+        //       break
+        //   }
+        // }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [text, dispatch]);
 
-  const { kucaAkt } = useGetAllKucaAktivnostiQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      kucaAkt: data?.user?.kuca.filter((akt) => {
-        if (text) {
-          if (
-            akt?.naziv.toLowerCase().includes(text.toLowerCase()) ||
-            akt?.opis.toLowerCase().includes(text.toLowerCase())
-          ) {
-            return akt
-          }
-        }
-      }),
-    }),
-  })
+    return (
+        <div className='justify-center flex-grow w-full'>
+            <div className='form-control lg:w-4/5'>
+                <input
+                    type='text'
+                    placeholder='Pretraga'
+                    className='input input-ghost'
+                    value={text}
+                    onChange={handleChange}
+                />
+            </div>
+        </div>
+    );
+};
 
-  const dispatch = useDispatch()
-
-  const handleChange = (e) => {
-    setText(e.target.value)
-    switch (router.pathname) {
-      case '/mama':
-        dispatch(showPretraga(mamaAkt))
-        break
-      case '/kuca':
-        dispatch(showPretraga(kucaAkt))
-        break
-      case '/posao':
-        dispatch(showPretraga(posaoAkt))
-        break
-      default:
-        break
-    }
-  }
-
-  useEffect(() => {
-    console.log(router.pathname)
-    dispatch(textPretrage(text))
-    // if (text === '') {
-    //   switch (router.pathname) {
-    //     case '/mama':
-    //       dispatch(showPretraga(mamaAkt))
-    //       break
-    //     case '/kuca':
-    //       break
-    //     case '/posao':
-    //       break
-    //     default:
-    //       break
-    //   }
-    // }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [text, dispatch])
-
-  return (
-    <div className='justify-center flex-grow w-full'>
-      <div className='form-control lg:w-4/5'>
-        <input
-          type='text'
-          placeholder='Pretraga'
-          className='input input-ghost'
-          value={text}
-          onChange={handleChange}
-        />
-      </div>
-    </div>
-  )
-}
-
-export default SearchComponent
+export default SearchComponent;
--- components/layout/racun/Card.jsx
+++ components/layout/racun/Card.jsx
@@ -1,84 +1,79 @@
-import { useRouter } from 'next/router'
-import { useGetStrujaQuery } from '../../../redux/api/strujaApi'
-import { useGetVodaQuery } from '../../../redux/api/vodaApi'
-import { useGetSmeceQuery } from '../../../redux/api/smeceApi'
-import { useGetMobitelQuery } from '../../../redux/api/mobitelApi'
-import { useGetKablovskaQuery } from '../../../redux/api/kablovskaApi'
-import { useGetIptvQuery } from '../../../redux/api/iptvApi'
+import { useRouter } from 'next/router';
+import { useGetStrujaQuery } from '../../../redux/api/strujaApi';
+import { useGetVodaQuery } from '../../../redux/api/vodaApi';
+import { useGetSmeceQuery } from '../../../redux/api/smeceApi';
+import { useGetMobitelQuery } from '../../../redux/api/mobitelApi';
+import { useGetKablovskaQuery } from '../../../redux/api/kablovskaApi';
+import { useGetIptvQuery } from '../../../redux/api/iptvApi';
 
 const Card = ({ racun, refs }) => {
-  const router = useRouter()
-  const { naziv, Ikona, path } = racun
+    const router = useRouter();
+    const { naziv, Ikona, path } = racun;
 
-  const { strujaZadnji } = useGetStrujaQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      strujaZadnji:
-        data?.user?.struja.length > 0
-          ? data?.user?.struja[data?.user?.struja.length - 1].mjesec
-          : '',
-    }),
-  })
-  const { vodaZadnji } = useGetVodaQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      vodaZadnji:
-        data?.user?.voda.length > 0
-          ? data?.user?.voda[data?.user?.voda.length - 1].mjesec
-          : '',
-    }),
-  })
-  const { smeceZadnji } = useGetSmeceQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      smeceZadnji:
-        data?.user?.smece.length > 0
-          ? data?.user?.smece[data?.user?.smece.length - 1].mjesec
-          : '',
-    }),
-  })
-  const { mobitelZadnji } = useGetMobitelQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      mobitelZadnji:
-        data?.user?.mobitel.length > 0
-          ? data?.user?.mobitel[data?.user?.mobitel.length - 1].mjesec
-          : '',
-    }),
-  })
-  const { kablovskaZadnji } = useGetKablovskaQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      kablovskaZadnji:
-        data?.user?.kablovska.length > 0
-          ? data?.user?.kablovska[data?.user?.kablovska.length - 1].mjesec
-          : '',
-    }),
-  })
-  const { iptvZadnji } = useGetIptvQuery(undefined, {
-    selectFromResult: ({ data }) => ({
-      iptvZadnji:
-        data?.user?.iptv.length > 0
-          ? data?.user?.iptv[data?.user?.iptv.length - 1].mjesec
-          : '',
-    }),
-  })
+    const { strujaZadnji } = useGetStrujaQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            strujaZadnji:
+                data?.user?.struja.length > 0
+                    ? data?.user?.struja[0].mjesec
+                    : '',
+        }),
+    });
 
-  return (
-    <div
-      onClick={() => router.push(path)}
-      className='flex items-center justify-center w-full p-4 text-center text-white transition-all shadow-2xl cursor-pointer shadow-slate-600/60 hover:bg-slate-500 lg:w-80 bg-slate-600 card h-28'
-    >
-      <div className='flex items-center space-x-5'>
-        <Ikona size={35} />
-        <p className='text-xl font-bold'>{naziv}</p>
-      </div>
-      <div className='flex items-center justify-between w-full mt-6'>
-        <i>Zadnji mjesec</i>
-        {refs === 'Struja' && <i>{strujaZadnji}</i>}
-        {refs === 'Voda' && <i>{vodaZadnji}</i>}
-        {refs === 'Smece' && <i>{smeceZadnji}</i>}
-        {refs === 'Mobitel' && <i>{mobitelZadnji}</i>}
-        {refs === 'Kablovska' && <i>{kablovskaZadnji}</i>}
-        {refs === 'IPTV' && <i>{iptvZadnji}</i>}
-      </div>
-    </div>
-  )
-}
+    const { vodaZadnji } = useGetVodaQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            vodaZadnji:
+                data?.user?.voda.length > 0 ? data?.user?.voda[0].mjesec : '',
+        }),
+    });
+    const { smeceZadnji } = useGetSmeceQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            smeceZadnji:
+                data?.user?.smece.length > 0 ? data?.user?.smece[0].mjesec : '',
+        }),
+    });
+    const { mobitelZadnji } = useGetMobitelQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            mobitelZadnji:
+                data?.user?.mobitel.length > 0
+                    ? data?.user?.mobitel[0].mjesec
+                    : '',
+        }),
+    });
+    const { kablovskaZadnji } = useGetKablovskaQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            kablovskaZadnji:
+                data?.user?.kablovska.length > 0
+                    ? data?.user?.kablovska[0].mjesec
+                    : '',
+        }),
+    });
+    const { iptvZadnji } = useGetIptvQuery(undefined, {
+        selectFromResult: ({ data }) => ({
+            iptvZadnji:
+                data?.user?.iptv.length > 0 ? data?.user?.iptv[0].mjesec : '',
+        }),
+    });
 
-export default Card
+    return (
+        <div
+            onClick={() => router.push(path)}
+            className='flex items-center justify-center w-full p-4 text-center text-white transition-all shadow-2xl cursor-pointer shadow-slate-600/60 hover:bg-slate-500 lg:w-80 bg-slate-600 card h-28'
+        >
+            <div className='flex items-center space-x-5'>
+                <Ikona size={35} />
+                <p className='text-xl font-bold'>{naziv}</p>
+            </div>
+            <div className='flex items-center justify-between w-full mt-6'>
+                <i>Zadnji mjesec</i>
+                {refs === 'Struja' && <i>{strujaZadnji}</i>}
+                {refs === 'Voda' && <i>{vodaZadnji}</i>}
+                {refs === 'Smece' && <i>{smeceZadnji}</i>}
+                {refs === 'Mobitel' && <i>{mobitelZadnji}</i>}
+                {refs === 'Kablovska' && <i>{kablovskaZadnji}</i>}
+                {refs === 'IPTV' && <i>{iptvZadnji}</i>}
+            </div>
+        </div>
+    );
+};
+
+export default Card;
